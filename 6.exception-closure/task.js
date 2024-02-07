133a1efae31683ae2552457f517ce1f8f4c5b0cc
Dmitrii <poltev.dmitrii.qa@gmail.com>
2024-02-07 10:55:06 +0300

    ДЗ  Обработка исключений и замыкания

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿// Задача №1
+function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+        throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;
+    }
+}
+
+// Пример использования
+console.log(validateCount("123")); // 123
+console.log(validateCount("abc")); // Error: Невалидное значение
+
+
+
+// Задача №2
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+              get perimeter() {
+                return "Ошибка! Треугольник не существует";
+              }, 
+            get area() {
+              return "Ошибка! Треугольник не существует";
+        }
+    }
+  }
+}
+
